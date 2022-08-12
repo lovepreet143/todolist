92bdf99c06a342cc4b60ff0966e33e14899c70fd
unknown <lovepreetsaggu45@gmaiul.com>
2022-08-12 19:55:30 -0400

    Add gitignore, procfile

--- app.js
+++ app.js
@@ -123,8 +123,10 @@ app.post("/delete", function(req, res){
 
 });
 
-
-
-app.listen(3000, function(){
-  console.log("Listening at port 3000");
+let port = process.env.PORT;
+if(port == null || port == ''){
+  port = 3000;
+}
+app.listen(port, function(){
+  console.log("Server has started Successfully");
 });
